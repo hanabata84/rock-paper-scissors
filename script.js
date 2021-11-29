1147738604bea255f1f8d73b4df1b9713ae1d00d
hanabata84 <dianneerika.busa@gmail.com>
2021-11-29 17:13:21 +0900

    logic finished

--- script.js
+++ script.js
@@ -1,3 +1,87 @@
+const rockButton = document.querySelector('.rockBtn');
+const paperButton = document.querySelector('.paperBtn');
+const scissorsButton = document.querySelector('.scissorsBtn');
+
+const compResult = document.querySelector('.compRes');
+const playerResult = document.querySelector('.playerRes');
+const roundResult = document.querySelector('.roundRes');
+const playerScoreP = document.querySelector('.playerScore');
+const computerScoreP = document.querySelector('.computerScore');
+const gameResult = document.querySelector('.gameRes');
+
+let playerScore = 0;
+let computerScore = 0;
+
+rockButton.addEventListener('click', playRock);
+paperButton.addEventListener('click', playPaper);
+scissorsButton.addEventListener('click', playScissors);
+
+function setText(playerScore, computerScore){
+    if(playerScore === 5 || computerScore === 5) {
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+        if(playerScore === 5) {
+            gameResult.textContent = 'YOU WIN!'
+        } else {
+            gameResult.textContent = 'YOU LOSE!'
+        }
+    }
+    playerScoreP.textContent = playerScore;
+    computerScoreP.textContent = computerScore;
+}
+
+function playRock(){
+    const computerSelection = computerPlay();
+    if (computerSelection === 'Rock') {
+        compResult.textContent = 'Rock';
+        roundResult.textContent = 'Aiko';
+    } else if (computerSelection === 'Paper') {
+        compResult.textContent = 'Paper';
+        roundResult.textContent = 'You lose! Paper > Rock';
+        computerScore += 1;
+    } else if (computerSelection === 'Scissors') {
+        compResult.textContent = 'Scissors';
+        roundResult.textContent = 'You win! Rock > Scissors';
+        playerScore += 1;
+    }
+    setText(playerScore, computerScore);
+}
+
+function playPaper(){
+    const computerSelection = computerPlay();
+    if (computerSelection === 'Rock') {
+        compResult.textContent = 'Rock';
+        roundResult.textContent = 'You win! Paper > Rock';
+        playerScore += 1;
+    } else if (computerSelection === 'Paper') {
+        compResult.textContent = 'Paper';
+        roundResult.textContent = 'Aiko';
+    } else if (computerSelection === 'Scissors') {
+        compResult.textContent = 'Scissors';
+        roundResult.textContent = 'You lose! Scissors > Paper';
+        computerScore += 1;
+    }
+    setText(playerScore, computerScore);
+}
+
+function playScissors(){
+    const computerSelection = computerPlay();
+    if (computerSelection === 'Rock') {
+        compResult.textContent = 'Rock';
+        roundResult.textContent = 'You lose! Rock > Scissors';
+        computerScore += 1;
+    } else if (computerSelection === 'Paper') {
+        compResult.textContent = 'Paper';
+        roundResult.textContent = 'You win! Scissors > Paper';
+        playerScore += 1;
+    } else if (computerSelection === 'Scissors') {
+        compResult.textContent = 'Scissors';
+        roundResult.textContent = 'Aiko';
+    }
+    setText(playerScore, computerScore);
+}
+
 function game() {
     let gameCount = 5;
     for(let i = 1; i <= gameCount; i++){
@@ -47,4 +131,4 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-game();
+// game();
