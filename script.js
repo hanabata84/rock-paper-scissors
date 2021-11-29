630efcb976c6330a15bb334977acc32bb5c4e90e
hanabata84 <dianneerika.busa@gmail.com>
2021-11-29 17:59:09 +0900

    refresh function added

--- script.js
+++ script.js
@@ -9,12 +9,19 @@ const playerScoreP = document.querySelector('.playerScore');
 const computerScoreP = document.querySelector('.computerScore');
 const gameResult = document.querySelector('.gameRes');
 
+const newRoundButton = document.querySelector('.newRound');
+
 let playerScore = 0;
 let computerScore = 0;
 
 rockButton.addEventListener('click', playRock);
 paperButton.addEventListener('click', playPaper);
 scissorsButton.addEventListener('click', playScissors);
+newRoundButton.addEventListener('click', refresh, false);
+
+function refresh(){
+    reload = location.reload();
+}
 
 function setText(playerScore, computerScore){
     if(playerScore === 5 || computerScore === 5) {
@@ -26,6 +33,7 @@ function setText(playerScore, computerScore){
         } else {
             gameResult.textContent = 'YOU LOSE!'
         }
+        newRoundButton.removeAttribute('hidden');
     }
     playerScoreP.textContent = playerScore;
     computerScoreP.textContent = computerScore;
@@ -82,21 +90,6 @@ function playScissors(){
     setText(playerScore, computerScore);
 }
 
-function game() {
-    let gameCount = 5;
-    for(let i = 1; i <= gameCount; i++){
-        const playerSelection = prompt('rock, paper, scissors');
-        const computerSelection = computerPlay();
-        if (playerSelection.toLowerCase() === 'rock' || playerSelection.toLowerCase() === 'paper' || playerSelection.toLowerCase() === 'scissors'){
-            console.log(playerSelection);
-            console.log(computerSelection);
-            console.log(playRound(playerSelection, computerSelection));
-        } else {
-            console.log('rock paper scissors only man! You lose the round!');
-        }
-    }
-}
-
 function computerPlay() {
     let randomPlay = Math.floor((Math.random() * 10));
     // random paper 0, 1, 2, 3, rock 4, 5, 6, scissors 7, 8, 9
@@ -108,27 +101,3 @@ function computerPlay() {
         return 'Scissors';
     }
 }
-
-function playRound(playerSelection, computerSelection){
-    if (playerSelection === 'rock' && computerSelection === 'Rock') {
-        return 'Aiko';
-    } else if (playerSelection === 'rock' && computerSelection === 'Paper') {
-        return 'You lose! Paper > Rock';
-    } else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
-        return 'You win! Rock > Scissors';
-    } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
-        return 'You lose! Paper > Rock';
-    } else if(playerSelection === 'paper' && computerSelection === 'Paper') {
-        return 'Aiko';
-    } else if(playerSelection === 'paper' && computerSelection === 'Scissors') {
-        return 'You lose! Scissors > Paper';
-    } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
-        return 'You lose! Rock > Scissors';
-    } else if(playerSelection === 'scissors' && computerSelection === 'Paper') {
-        return 'You win! Scissors > Paper';
-    } else if(playerSelection === 'scissors' && computerSelection === 'Scissors') {
-        return 'Aiko';
-    }
-}
-
-// game();
